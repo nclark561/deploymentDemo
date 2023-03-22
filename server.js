45c1cd5d7c0157cf647822c03b9c880a3c6c5c84
Noah Clark <noahammon00@gmail.com>
2023-03-22 15:27:57 -0600

    uses static middleware instead of traditional endpoints

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-
-app.get('/css', (req, res) => {
-    res.sendFile(__dirname + '/public/styles.css')
-})
-
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname + '/public/main.js')
-})
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000, () => console.log('app up on 4000')) 
