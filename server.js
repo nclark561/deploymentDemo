07c4896d7455dd890d7362b9d5aa2cd3ecff2d49
Noah Clark <noahammon00@gmail.com>
2023-03-22 11:58:43 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(5150, () => console.log('app up on 5150')) 
